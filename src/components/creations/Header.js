f7caf2988e80e739c9870de8eb557dcadce66115
Aqib <chaqib4u@gmail.com>
2024-06-13 03:36:19 +0500

    color changes

--- src/components/creations/Header.js
+++ src/components/creations/Header.js
@@ -4,9 +4,9 @@ const Header = () => {
   return (
 <>
 <section className="max-w-[1440px] mx-auto ">
-    <div className="flex justify-between py-[22px] px-6 lg:px-[120px]">
+    <div className="flex justify-between  py-[22px] pl-6 pr-[0px] lg:px-[120px]">
      <div className="flex flex-row gap-[50px]">
-     <p className="font-[400] text-[28px] leading-[35.7px] text-[#E57050] cursor-pointer">
+     <p className="font-[400] text-[28px] leading-[35.7px] bg-clip-text text-transparent bg-gradient-to-r from-[#E57050] to-[#DB2268] cursor-pointer">
         ART GENERATOR
       </p>
       <div className="hidden lg:block">
@@ -26,9 +26,10 @@ const Header = () => {
         </div>
       </div>
      </div>
-      <button className="hidden md:block text-[16px] font-[500] text-[#FFFFFF] leading-[27px] px-[29px] pt-[11px] pb-[11px] bg-gradient-to-r from-[#E57050] to-[#DB2268]  rounded-[12px] cursor-pointer">
+      <button className="hidden lg:block text-[16px] font-[500] text-[#FFFFFF] leading-[27px] px-[29px] pt-[11px] pb-[11px] bg-gradient-to-r from-[#E57050] to-[#DB2268]  rounded-[12px] cursor-pointer">
         Sign In
       </button>
+      <img src='./Images/man.png' alt='loading...' className='lg:hidden block w-[20px] h-[20px] mt-[10px]'/>
     </div>
     
   </section>
