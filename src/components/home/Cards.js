50d302b1383754a573157718ccfaf2e78aae1439
Aqib <chaqib4u@gmail.com>
2024-06-14 05:49:40 +0500

    add scroll up

--- src/components/home/Cards.js
+++ src/components/home/Cards.js
@@ -3,11 +3,17 @@ import ImageGrid from "./ImageGrid";
 import ImageGridMobile from "./ImageGridMobile";
 
 const Cards = () => {
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" }); // Scrolls to the top of the page smoothly
+  };
+
   return (
     <section className="max-w-[1440px] mx-auto mt-[52px] px-6 lg:px-[120px] relative ">
-      <div className="flex justify-end cursor-pointer absolute right-[60px] bottom-[-35px] ">
+      <div
+        className="flex justify-end cursor-pointer absolute right-[60px] bottom-[-35px]"
+        onClick={handleScrollToTop}
+      >
         <img src="./Images/ArrowUp.png" alt="up arrow" />
-        
       </div>
       <div className="hidden md:block">
         <ImageGrid />
