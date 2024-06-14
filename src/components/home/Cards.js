eb378fcbaa6763b1c3a8ddefe5d260f5b831d607
Aqib <chaqib4u@gmail.com>
2024-06-14 05:46:49 +0500

    add arrow

--- src/components/home/Cards.js
+++ src/components/home/Cards.js
@@ -5,6 +5,10 @@ import ImageGridMobile from "./ImageGridMobile";
 const Cards = () => {
   return (
     <section className="max-w-[1440px] mx-auto mt-[52px] px-6 lg:px-[120px] relative ">
+      <div className="flex justify-end cursor-pointer absolute right-[60px] bottom-[-35px] ">
+        <img src="./Images/ArrowUp.png" alt="up arrow" />
+        
+      </div>
       <div className="hidden md:block">
         <ImageGrid />
       </div>
